d5e64055eb9987bcccb1e93694a88594d5df4de6
Mohammad Shamsi <m.h.shams@gmail.com>
2015-05-27 23:34:56 +0200

    a potential fluent api

---
+++ src/main/java/org/jnbis/api/Jnbis.java
@@ -0,0 +1,25 @@
+package org.jnbis.api;
+
+/**
+ *
+ */
+public final class Jnbis {
+
+}
+
+/*
+ Jnbis.wsq().decode("my file").toPng().asInputStream()
+ Jnbis.wsq().decode("my file").toGif().asByteArray()
+ Jnbis.wsq().decode("my file").toJpeg().save("file")
+ Jnbis.wsq().decode("my file").toBmp().save(new File(""))
+*/
+
+/*
+ decoded = Jnbis.nist().decode("my file")
+ decoded.bitmaps().get(0).toPng().save("file")
+ decoded.bitmaps().keys()
+ decoded.bitmaps().values()
+ decoded.texts().get(1)
+ decoded.texts().keys()
+ decoded.texts().values()
+ */
